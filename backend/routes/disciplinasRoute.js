fe3694092998989ee652373323ff968e611fee28
micapareddes <micapareddes@gmail.com>
2024-06-26 10:14:28 -0300

    Add routes for listing and creating disciplines in disciplinasRoute.js

--- backend/routes/disciplinasRoute.js
+++ backend/routes/disciplinasRoute.js
@@ -6,6 +6,7 @@ import { authenticateToken } from "../middleware/authenticateToken.js"
 const routerDisciplina = Router()
 
 routerDisciplina.get("/", authenticateToken, tryCatch(DisciplinaController.consultarDisciplinasPorProfessor))
-routerDisciplina.post("/", tryCatch(DisciplinaController.criarDisciplina))
+routerDisciplina.get("/cadastradas", authenticateToken, tryCatch(DisciplinaController.listarDisciplinasCadastradas))
+routerDisciplina.post("/", authenticateToken, tryCatch(DisciplinaController.criarDisciplina))
 
 export default routerDisciplina
