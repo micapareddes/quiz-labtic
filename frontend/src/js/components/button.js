d7975ad9efa6c9efcaadb250a1d4b74bb1208227
micapareddes <micapareddes@gmail.com>
2024-07-12 15:57:28 -0300

    Possibilidade do componente de botão ser um link adicionada

--- frontend/src/js/components/button.js
+++ frontend/src/js/components/button.js
@@ -1,24 +1,24 @@
 function Button( 
-    { variant='primary', size='md', title='Button', destructive=false, icon=false, disabled=false, type='button', onClick }
+    { variant='primary', size='md', title='Button', destructive=false, icon=false, disabled=false, type='button', onClick, link=null }
 ) {
-    const isLink = variant === 'link'
+    const isGhost = variant === 'ghost'
     const sizeStyle = {
-        'sm': isLink ? 'text-sm' : 'px-5 py-2',
-        'md': isLink ? 'text-base' : 'px-7 py-3',
-        'lg': isLink ? 'text-lg' : 'w-64 py-3',
-        'full': isLink ? 'text-xl' : 'w-full py-3'
+        'sm': isGhost ? 'text-sm' : 'px-5 py-2',
+        'md': isGhost ? 'text-base' : 'px-7 py-3',
+        'lg': isGhost ? 'text-lg' : 'w-64 py-3',
+        'full': isGhost ? 'text-xl' : 'w-full py-3'
     }
     const variantStyle = {
         'primary': 'bg-indigo-500 text-neutral-50 data-[destructive="true"]:bg-red-500',
 
         'outline': 'border border-stone-400 text-stone-500 data-[destructive="true"]:border-red-500 data-[destructive="true"]:text-red-500',
 
-        'link': 'text-indigo-700 data-[destructive="true"]:text-red-500 hover:underline underline-offset-4'
+        'ghost': 'text-indigo-700 data-[destructive="true"]:text-red-500 hover:underline underline-offset-4'
     }
 
-    const button = createHTMLElement('button')
+    const button = link ? createHTMLElement('a') : createHTMLElement('button')
     button.dataset.destructive = destructive
-    button.type = type
+    if (!link) button.type = type
     button.textContent = title
     button.className = sizeStyle[size] + ' ' +  variantStyle[variant] + ' ' + 'rounded-md'
 
@@ -39,12 +39,11 @@ function Button(
         button.classList.add('flex', 'items-center', 'justify-center', 'gap-2')
     }
 
-    button.addEventListener('click', () => {
+    if (link) button.href = link
+
+    if (!link) button.addEventListener('click', () => {
         onClick()
     })
 
     return button
 }
-
-const main = document.getElementById('body')
-main.appendChild(Button({variant: 'outline', size: 'full', destructive:true}))
