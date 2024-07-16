3c2ecfd23983fc38974e23f47a86a91bb423c663
micapareddes <micapareddes@gmail.com>
2024-07-16 10:18:53 -0300

    Arquivo centralizado para exportação de componentes

---
+++ frontend/src/js/components/components.js
@@ -0,0 +1,18 @@
+import Button from './button.js'
+import Heading from './heading.js'
+import Tag from './tag.js'
+import { AlertDialog, ActionDialog, openDialog } from './dialog.js'
+import { SuccessToaster, InfoToaster, openToaster, closeToaster } from './toaster.js'
+
+export { 
+    Button, 
+    AlertDialog, 
+    ActionDialog, 
+    openDialog, 
+    Heading, 
+    Tag, 
+    SuccessToaster, 
+    InfoToaster, 
+    openToaster, 
+    closeToaster 
+}
