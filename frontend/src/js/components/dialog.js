407d598eb966ef4662b9bf2d3066e0b736b4ae2f
micapareddes <micapareddes@gmail.com>
2024-07-18 17:35:10 -0300

    Componente de Success Dialog

--- frontend/src/js/components/dialog.js
+++ frontend/src/js/components/dialog.js
@@ -1,3 +1,5 @@
+import { Button } from './button.js'
+
 function constructorDialog({ title, message, confirmarButtonName = 'Confirmar', onConfirm, color='neutral' }) {
     const container = createHTMLElement('dialog')
     const div = createHTMLElement('div')
@@ -64,6 +66,50 @@ export function ActionDialog({ title, message, confirmarButtonName }) {
         onConfirm, 
         color: 'indigo' 
     })
+}
+
+export function SuccessDialog({ 
+    title='Título', message='Mensagem do dialog', buttonName='Redirecionar', link,
+}) {
+    const container = createHTMLElement('dialog')
+    const dialog = createHTMLElement('dialog')
+    const headingContainer = createHTMLElement('header')
+    const icon = createHTMLElement('img')
+    const dialogTitle = createHTMLElement('h4')
+    const dialogMessage = createHTMLElement('p')
+    const closeButton = createHTMLElement('button')
+    const redirectButton = Button({
+        variant: 'ghost',
+        title: buttonName,
+        link: link,
+    })
+
+    container.className = 'absolute left-0 top-0 bg-indigo-950 bg-opacity-50 backdrop-blur-sm h-screen w-screen flex items-center justify-center'
+
+    dialog.className = 'bg-neutral-50 rounded-3xl px-6 py-8'
+
+    headingContainer.className = 'flex flex-col gap-3'
+
+    icon.src = '/frontend/src/img/icones/check-circle.svg'
+    icon.height = 32
+    icon.width = 32
+
+    dialogTitle.textContent = title
+    dialogTitle.className = 'text-xl lg:text-2xl font-semibold text-stone-900 mb-4'
+
+    dialogMessage.textContent = message
+    dialogMessage.className = 'mb-6 text-stone-700 text-base w-64 md:w-[350px] lg:w-[479px]'
+
+    closeButton.src = '/frontend/src/img/icones/x.svg'
+    closeButton.width = 21
+    closeButton.height = 21
+    closeButton.className = 'opacity-20'
+
+    headingContainer.append(icon, dialogTitle)
+    dialog.append(headingContainer, dialogMessage, redirectButton)
+    container.appendChild(dialog)
+
+    return container
 }
 
 export function openDialog({ dialog, rootId }) {
