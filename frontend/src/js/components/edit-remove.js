80b0158d68343e2ac0faa8262bd7c57808935437
micapareddes <micapareddes@gmail.com>
2024-07-23 14:01:26 -0300

    Componente de botões 'editar' e 'remover' criado

---
+++ frontend/src/js/components/edit-remove.js
@@ -0,0 +1,20 @@
+import { Button } from "./button.js"
+
+export function EditRemoveActionButtons({ onEdit, onRemove }) {
+    const container = createHTMLElement('div')
+    const editButton = Button({
+        variant: 'ghost',
+        title: 'Editar',
+        link: onEdit,
+    })
+    const removeButton = Button({
+        variant: 'ghost',
+        title: 'Remover',
+        link: onRemove,
+    })
+
+    container.className = 'flex flex-row gap-8'
+    container.append(editButton, removeButton)
+
+    return container
+}
