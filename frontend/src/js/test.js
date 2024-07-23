3936f759b4fef6a1d7cf0ce96bb6a1b5eca1762c
micapareddes <micapareddes@gmail.com>
2024-07-23 14:05:27 -0300

    Update de output.css e adicionados novos componentes em pagina de componentes; Trocada cor de bg da pagina de componentes

--- frontend/src/js/test.js
+++ frontend/src/js/test.js
@@ -1,9 +1,47 @@
+import { Button } from './components/button.js'
 import { Sidebar } from './components/sidebar.js'
 import { QuestionSidecard, AttemptsSidecard } from './components/sidecard.js'
+import { Tag } from './components/tag.js'
+import { Heading } from './components/heading.js'
+import { TooltipInfo } from './components/tooltip-info.js'
+import { 
+    ListItemBox, ListItemBoxWithTitle, QuizListItem, EditQuizListItem, RegisterListItem, StudentGradeListItem 
+} from './components/list.js'
 
-const sidebarSection = document.getElementById('sidebar')
-const sidecarSection = document.getElementById('sidecar')
+// Botões
+const primary = document.getElementById('button-primary')
+const outline = document.getElementById('button-outline')
+const othersButton = document.getElementById('button-others')
 
+primary.append(
+    Button({}),
+    Button({ size: 'sm' }),
+    Button({ size: 'lg' }),
+    Button({ size: 'full' }),
+)
+outline.append(
+    Button({ variant: 'outline' }),
+    Button({ variant: 'outline', size: 'sm' }),
+    Button({ variant: 'outline', size: 'lg' }),
+    Button({ variant: 'outline', size: 'full' }),
+)
+othersButton.append(
+    Button({ size: 'lg', destructive: true }),
+    Button({ destructive: true }),
+    Button({ size: 'sm', destructive: true }),
+    Button({ size: 'lg', variant: 'outline', destructive: true }),
+    Button({ variant: 'outline', destructive: true }),
+    Button({ size: 'sm', variant: 'outline', destructive: true }),
+    Button({ size: 'lg', variant: 'ghost', destructive: true }),
+    Button({ variant: 'ghost', destructive: true }),
+    Button({ size: 'sm', variant: 'ghost', destructive: true }),
+    Button({ variant: 'ghost', size: 'lg' }),
+    Button({ variant: 'ghost', size: 'md' }),
+    Button({ variant: 'ghost', size: 'sm' }),
+)
+
+// Sidebar
+const sidebar = document.getElementById('sidebar')
 const items = [
     {
         icon: '/frontend/src/img/icones/house.svg',
@@ -40,7 +78,13 @@ const items = [
         ],
     },
 ]
+sidebar.append(
+    Sidebar({ size: 'lg', items: items }),
+    Sidebar({ size: 'sm', items: items }),
+)
 
+// Sidecard
+const sidecar = document.getElementById('sidecar')
 const respostas = [
     {
         question: 'Pergunta 1',
@@ -74,7 +118,6 @@ const respostas = [
         answer: 'A',
     },
 ]
-
 const gabarito = [
     {
         question: 'Pergunta 1',
@@ -126,13 +169,7 @@ const gabarito = [
 
     },
 ]
-
-sidebarSection.append(
-    Sidebar({ size: 'lg', items: items }),
-    Sidebar({ size: 'sm', items: items }),
-)
-
-sidecarSection.append(    
+sidecar.append(    
     QuestionSidecard({
     questions: respostas,
     title: 'Respostas',
@@ -163,4 +200,33 @@ sidecarSection.append(
         ]
     }),
     AttemptsSidecard({})
+)
+
+// List
+const list = document.getElementById('list')
+list.append(
+    ListItemBox({}),
+    ListItemBoxWithTitle({}),
+    QuizListItem({
+        name: 'Simulado de Poções Básicas e Avançadas',
+        endDate: '1 de Janeiro',
+        type: 'exc'
+    }),
+    QuizListItem({
+        name: 'Prova de Poções Básicas',
+        endDate: '17 de Fevereiro',
+        type: 'exc'
+    }),
+    EditQuizListItem({}),
+    RegisterListItem({ name: 'Professor' }),
+    StudentGradeListItem({})
+)
+
+// Others
+const others = document.getElementById('others')
+others.append(
+    Tag({}),
+    Heading({ goBack: true, subtitle: 'Subtitle' }),
+    Heading({ subtitle: 'Subtitle' }),
+    TooltipInfo({}),
 )
