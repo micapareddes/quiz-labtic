b7f331ab60d67d458f8ede380313f2c09215d8a4
micapareddes <micapareddes@gmail.com>
2024-07-25 11:07:06 -0300

    Adicionada propriedade para definir grossura da fonte em componente Text e prop de className para adicionar estilização adicional

--- frontend/src/js/components/fonts.js
+++ frontend/src/js/components/fonts.js
@@ -26,7 +26,7 @@ export function Title({ title='Titulo', size='xl', tone='s-900', bold='semibold'
     return titulo
 }
 
-export function Text({ text='Texto', size='base', tone='s-500', bold=false, as='p' }) {
+export function Text({ text='Texto', size='base', tone='s-500', bold='normal', as='p', className }) {
     const font = {
         'sm': 'text-sm',
         'md': 'text-base',
@@ -36,14 +36,21 @@ export function Text({ text='Texto', size='base', tone='s-500', bold=false, as='
     const color = {
         'i-50': 'text-indigo-50',
         'i-700': 'text-indigo-700',
+        's-400': 'text-stone-400',
         's-500': 'text-stone-500',
         's-600': 'text-stone-600',
         's-700': 'text-stone-700',
         's-900': 'text-stone-900',
     }
 
+    const weight = {
+        'bold': 'font-bold',
+        'semibold': 'font-semibold',
+        'regular': 'font-normal',
+    }
+
     const texto = createHTMLElement(as)
     texto.textContent = text
-    texto.className = `${font[size]} ${color[tone]} ${bold && 'font-semibold'}`
+    texto.className = `${font[size]} ${color[tone]} ${weight[bold]} ${className}`
     return texto
 }
