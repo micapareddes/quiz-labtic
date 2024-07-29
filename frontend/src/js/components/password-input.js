463f4ab246e0f0307bdf456cb8caf0d533ebea11
micapareddes <micapareddes@gmail.com>
2024-07-29 15:15:23 -0300

    Criado componente de input de senha

---
+++ frontend/src/js/components/password-input.js
@@ -0,0 +1,41 @@
+export function PasswordInput({ className }) {
+    const container = createHTMLElement('div')
+    const inputContainer = createHTMLElement('div')
+    const label = createHTMLElement('label')
+    const input = createHTMLElement('input')
+    const eyeButton = createHTMLElement('button')
+    const eyeIcon = createHTMLElement('img')
+
+    inputContainer.className = 'flex w-full px-2 py-3 rounded-md bg-neutral-100 border border-neutral-200 mt-3 has-[:focus]:ring-2 has-[:focus]:ring-yellow-400'
+
+    input.type = 'password'
+    input.name = 'password'
+    input.id = 'password'
+    input.placeholder = '••••••••'
+    input.className = `peer w-full mr-2 bg-transparent focus:outline-none ${className}`
+
+    label.textContent = 'Senha'
+    label.htmlFor = 'password'
+    label.className = 'block'
+
+    eyeIcon.src = '/frontend/src/img/icones/eye.svg'
+
+    eyeButton.setAttribute('aria-label', 'Mostrar ou ocultar senha') 
+    eyeButton.addEventListener("click", (event) => {
+        event.preventDefault()
+
+        if (input.type === 'password') {
+            input.type = 'text'
+            eyeIcon.src = '/frontend/src/img/icones/eye-slash.svg'
+        } else {
+            input.type = 'password'
+            eyeIcon.src = '/frontend/src/img/icones/eye.svg'
+        }
+    })
+
+    eyeButton.appendChild(eyeIcon)
+    inputContainer.append(input, eyeButton)
+    container.append(label, inputContainer)
+
+    return container
+}
