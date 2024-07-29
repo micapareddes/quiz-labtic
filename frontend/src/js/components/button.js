7d220beb0bd3acda2572d1abcd284758c309f514
micapareddes <micapareddes@gmail.com>
2024-07-29 13:07:39 -0300

    Adicionado hover no componente de botão

--- frontend/src/js/components/button.js
+++ frontend/src/js/components/button.js
@@ -9,9 +9,9 @@ export function Button(
         'full': isGhost ? 'text-xl' : 'w-full py-3'
     }
     const variantStyle = {
-        'primary': 'bg-indigo-500 text-neutral-50 data-[destructive="true"]:bg-red-500',
+        'primary': 'bg-indigo-500 hover:bg-indigo-400 transition-all duration-100 text-neutral-50 data-[destructive="true"]:bg-red-500 data-[destructive="true"]:hover:bg-red-400',
 
-        'outline': 'border border-stone-400 text-stone-500 data-[destructive="true"]:border-red-500 data-[destructive="true"]:text-red-500',
+        'outline': 'border border-stone-400 hover:bg-stone-400 hover:bg-opacity-10 text-stone-500 transition-all duration-100 data-[destructive="true"]:border-red-500 data-[destructive="true"]:text-red-500 data-[destructive="true"]:hover:bg-red-500 data-[destructive="true"]:hover:bg-opacity-10',
 
         'ghost': 'text-indigo-700 data-[destructive="true"]:text-red-500 hover:underline underline-offset-4'
     }
@@ -20,11 +20,12 @@ export function Button(
     button.dataset.destructive = destructive
     if (!link) button.type = type
     button.textContent = title
-    button.className = sizeStyle[size] + ' ' +  variantStyle[variant] + ' ' + 'rounded-md'
+    button.className = sizeStyle[size] + ' ' +  variantStyle[variant] + ' ' + 'rounded-md cursor-pointer'
 
     if (disabled) {
        button.disabled = true
-       button.classList.add('opacity-50')
+       button.classList.remove('hover:bg-stone-400', 'hover:bg-opacity-10', 'hover:underline', 'data-[destructive="true"]:hover:bg-red-400', 'data-[destructive="true"]:hover:bg-red-500', 'hover:bg-none')
+       button.classList.add('opacity-50', 'cursor-not-allowed')
     }
 
     if (icon) {
