66cff1df27a34655dfb3e5b43e7c4aefbfd1dcba
micapareddes <micapareddes@gmail.com>
2024-07-29 13:07:15 -0300

    Resolvido bug com subtitulo e seta para voltar no componente heading

--- frontend/src/js/components/heading.js
+++ frontend/src/js/components/heading.js
@@ -8,27 +8,34 @@ export function Heading({ title='Titulo', subtitle='', subtitleSize='lg', goBack
         tone: 's-900',
     })
     const eSubtitle = Title({ 
-        subtitle,
+        title: subtitle,
         size: subtitleSize,
         tone: 's-700',
-        bold: 'regular'
+        bold: 'regular',
     })
 
-    heading.className = 'grid'
+    heading.className = 'w-full'
     heading.append(eTitle, eSubtitle)
 
     if (goBack) {
-        heading.classList.add('grid-heading', 'items-start')
+        heading.classList.add('grid-heading', 'items-center', 'justify-start')
+        eSubtitle.classList.add('row-start-2', 'col-start-2')
 
         const goBackButton = createHTMLElement('button')
         const leftArrow = createHTMLElement('img')
+        const div = createHTMLElement('div')
+
+        div.className = 'flex justify-start w-8 h-8 mr-3'
 
         leftArrow.src = '/frontend/src/img/icones/caret-left.svg'
+        leftArrow.height = 32
+        leftArrow.width = 32
 
-        goBackButton.className = 'row-span-2 mt-1 mr-4 hover:bg-neutral-200 rounded-md'
+        goBackButton.className = 'row-span-2 hover:bg-neutral-200 rounded-md p-1'
         goBackButton.appendChild(leftArrow)
+        div.appendChild(goBackButton)
 
-        heading.prepend(goBackButton)
+        heading.prepend(div)
 
         goBackButton.addEventListener('click', () => {
             onGoBack()
