659c132abb57ef88dd67fe193e79749d8a14a7c8
micapareddes <micapareddes@gmail.com>
2024-07-31 20:32:15 -0300

    Adicionadas importações necessarias em check and redirect user e user type function

--- frontend/src/js/auth/checkAndRedirectUser.js
+++ frontend/src/js/auth/checkAndRedirectUser.js
@@ -1,3 +1,6 @@
+import { reqUserType } from '/frontend/src/js/functions/userType.js'
+import { redirectToUserDashboard } from '/frontend/src/js/functions/redirectToUserDashboard.js'
+
 export async function checkAndRedirectUser() {
     const accessToken = localStorage.getItem('accessToken')
     if (accessToken) {
--- frontend/src/js/functions/userType.js
+++ frontend/src/js/functions/userType.js
@@ -1,5 +1,6 @@
+import { makeRequest } from '/frontend/src/js/functions/makeRequest.js'
 export async function reqUserType() {
     const url = 'http://localhost:3333/api/usuarios/me'
-    const token = getFromLocalStorage('accessToken')
+    const token = localStorage.getItem('accessToken')
     return await makeRequest({ url, method: 'GET', token: token })
 }
