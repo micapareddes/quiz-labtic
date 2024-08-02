137cdfb851456de6001ecaed9508fa7a27d54d12
micapareddes <micapareddes@gmail.com>
2024-08-02 18:01:50 -0300

    Adicionado id no container do componente de text input

--- frontend/src/js/components/text-input.js
+++ frontend/src/js/components/text-input.js
@@ -14,6 +14,7 @@ export function TextInput({
     const input = document.createElement('input')
 
     container.className = 'space-y-3'
+    container.id = `${id}-container`
 
     input.type = type
     input.name = id
