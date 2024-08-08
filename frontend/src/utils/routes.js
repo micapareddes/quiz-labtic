258a9b53b1541f080ab4c0e66f241276f76d9209
micapareddes <micapareddes@gmail.com>
2024-08-08 15:10:41 -0300

    Adicionadas rotas de endpoints em routes.js

--- frontend/src/utils/routes.js
+++ frontend/src/utils/routes.js
@@ -20,3 +20,18 @@ export const ROUTES = {
         DASHBOARD: 'http://localhost:5500/frontend/src/pages/professor/dashboard.html',
     },
 }
+
+export const API_ENDPOINTS = {
+    GET_DISCIPLINAS: 'http://localhost:3333/api/disciplinas/cadastradas',
+    GET_DISCIPLINA_BY_ID: (id) => `http://localhost:3333/api/disciplinas/${id}`,
+    POST_DISCIPLINA: 'http://localhost:3333/api/disciplinas',
+    PATCH_DISCIPLINA: (id) => `http://localhost:3333/api/disciplinas/${id}`,
+    DELETE_DISCIPLINA: 'http://localhost:3333/api/disciplinas',
+
+    GET_USER_TYPE: 'http://localhost:3333/api/usuarios/me',
+    GET_USER_NAME: 'http://localhost:3333/api/usuarios/name',
+
+    GET_PROFESSORES: 'http://localhost:3333/api/usuarios/professores',
+    
+    DELETE_RELATION: 'http://localhost:3333/api/alunos_disciplinas',
+}
