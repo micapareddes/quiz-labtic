92502f6378c69e415ebecf9d370717f3077cf370
micapareddes <micapareddes@gmail.com>
2024-08-08 14:37:18 -0300

    Criada pagina de routes para centralizar rotas do aplicativo

---
+++ frontend/src/utils/routes.js
@@ -0,0 +1,15 @@
+export const ROUTES = {
+    LOGIN: '/frontend/src/pages/login/login.html',
+    ADMIN: {
+        DASHBOARD: '/frontend/src/pages/admin/dashboard/dashboard.html',
+        PAINEL: {
+            DISCIPLINAS: '/frontend/src/pages/admin/painel/disciplinas/disciplinas.html' 
+        },
+        CADASTRTO: {
+            DISCIPLINAS: '/frontend/src/pages/admin/cadastro/disciplinas/disciplinas.html'
+        },
+        EDICAO: {
+            DISCIPLINAS: '/frontend/src/pages/admin/edicao/disciplinas/disciplinas.html'
+        },
+    }
+}
