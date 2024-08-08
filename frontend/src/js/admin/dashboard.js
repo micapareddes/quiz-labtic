d05a64b011c7463ef33efc57f6822d58456d3fb5
micapareddes <micapareddes@gmail.com>
2024-08-08 14:37:04 -0300

    Refatoração na arquitetura de pastas

--- frontend/src/js/admin/dashboard.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    const accessToken = getFromLocalStorage('accessToken')
-    const nameReq = await makeRequest( { url: 'http://localhost:3333/api/usuarios/name', method:'GET', token: accessToken})
-    
-    const name = nameReq.name
-    
-    verifyUserAccess('admin')
-    mostrarNomeUsuario(name)
-})
