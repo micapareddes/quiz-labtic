eaa14a1d499ed75f350555a7bb580cc4d98d1ae7
micapareddes <micapareddes@gmail.com>
2024-08-08 14:50:51 -0300

    Adicoinadas rotas de dashboard de aluno e de professor; e rota de 404 em routes.js

--- frontend/src/utils/routes.js
+++ frontend/src/utils/routes.js
@@ -1,7 +1,8 @@
 export const ROUTES = {
     LOGIN: '/frontend/src/pages/login/login.html',
+    ERROR404: '/frontend/src/old-pages/404.html',
     ADMIN: {
-        DASHBOARD: '/frontend/src/pages/admin/dashboard/dashboard.html',
+        DASHBOARD: '/frontend/src/pages/admin/dashboard/index.html',
         PAINEL: {
             DISCIPLINAS: '/frontend/src/pages/admin/painel/disciplinas/disciplinas.html' 
         },
@@ -11,5 +12,11 @@ export const ROUTES = {
         EDICAO: {
             DISCIPLINAS: '/frontend/src/pages/admin/edicao/disciplinas/disciplinas.html'
         },
-    }
+    },
+    ALUNO: {
+        DASHBOARD: '/frontend/src/old-pages/aluno/dashboard.html',
+    },
+    PROFESSOR: {
+        DASHBOARD: 'http://localhost:5500/frontend/src/pages/professor/dashboard.html',
+    },
 }
