a3aff766408f41f1c354ebf4f32c166cf7d89a50
micapareddes <micapareddes@gmail.com>
2024-08-08 23:20:01 -0300

    Corregida rota de login em funcao signOut utilizando routes.js

--- frontend/src/auth/singOut.js
+++ frontend/src/auth/singOut.js
@@ -1,10 +1,11 @@
+import { ROUTES } from '/frontend/src/utils/routes.js'
 const buttonEncerrarSessao = document.getElementById('encerrar-sessao-button')
 const buttonCancelarDialogEncerrarSessao = document.getElementById('cancelar-button-encerrar-sessao-dialog')
 const buttonConfirmarDialogEncerrarSessao = document.getElementById('confirmar-button-encerrar-sessao-dialog')
 
 export function signOut() {
     localStorage.removeItem('accessToken')
-    window.location.href = 'http://localhost:5500/frontend/src/pages/login.html'
+    window.location.href = ROUTES.LOGIN
 }
 
 // buttonEncerrarSessao.addEventListener('click', () => {
