42db1163c63ddb015cd41cfa7079ef07a2e2b698
micapareddes <micapareddes@gmail.com>
2024-08-12 10:53:22 -0300

    Criado pagina de painel de alunos

---
+++ frontend/src/pages/admin/painel/alunos/page.js
@@ -0,0 +1,76 @@
+// Functions
+import { ROUTES } from '/frontend/src/utils/routes.js'
+import { verifyUserAccess } from '/frontend/src/auth/verifyUserAccess.js'
+import { getDisciplinas } from '/frontend/src/pages/admin/painel/disciplinas/service/getDisciplinas.js'
+import { navigateTo } from '/frontend/src/functions/navigateTo.js'
+import { saveWindowPath } from '/frontend/src/functions/saveWindowPath.js'
+
+// Components
+import { Heading } from '/frontend/src/components/heading.js'
+import { SidebarAdmin } from '/frontend/src/pages/admin/components/sidebar-admin.js'
+import { UsersTable } from '/frontend/src/pages/admin/painel/components/users-table.js'
+import { Button } from '/frontend/src/components/button.js'
+import { Empty } from '/frontend/src/components/empty.js'
+import { SuccessToaster, openToaster, closeToaster } from '/frontend/src/components/toaster.js'
+
+async function AlunosPage() {
+    verifyUserAccess('admin')
+    const root = document.getElementById('root')
+    const main = document.getElementById('main')
+    const header = document.createElement('div')
+    const alunos = [
+        {
+            id: 'ABC123456FD',
+            matricula: '123456',
+            name: 'Harry Potter',
+            disciplinas: [],
+            toEdit: '',
+            toRemove: '',
+        }
+    ]
+    // const alunosFormatados = parseUsers(alunos)
+    const quantidadeAlunos = alunos.length
+
+    header.className = 'flex flex-row justify-between items-start mb-10'
+    header.append(        
+        Heading({
+            goBack: true, 
+            onGoBack: () => navigateTo(ROUTES.ADMIN.DASHBOARD),
+            title: 'Alunos', 
+            subtitle: `${quantidadeAlunos} cadastradas`,
+            subtitleSize: 'lg',
+        }),
+        Button({
+            variant: 'primary',
+            size: 'md',
+            title: 'Cadastrar',
+            icon: true,
+            link: ROUTES.ADMIN.CADASTRO.ALUNOS
+        })
+    )
+    root.prepend(SidebarAdmin())
+
+    main.append(header)
+
+    if (quantidadeAlunos === 0) {
+        main.appendChild(
+            Empty('Não há alunos cadastrados.')
+        )
+    } else {
+        main.appendChild(
+            UsersTable({ type: 'aluno', rows: alunos})
+        )
+        const alunoAlterado = localStorage.getItem('alunoAlterado')
+        if (alunoAlterado) {
+            openToaster(
+                SuccessToaster({
+                    message: `Alterações do aluno "${alunoAlterado}" salvas!`
+                })
+            )
+            closeToaster()
+            localStorage.removeItem('alunoAlterado')
+        }
+    }
+    saveWindowPath()
+}
+AlunosPage()
