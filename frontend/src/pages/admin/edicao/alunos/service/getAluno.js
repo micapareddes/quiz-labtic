6cdf6710a347aa0ca7c2b08f928b12b1fa7318d2
micapareddes <micapareddes@gmail.com>
2024-08-20 14:59:47 -0300

    Criada função de requisição de get aluno

---
+++ frontend/src/pages/admin/edicao/alunos/service/getAluno.js
@@ -0,0 +1,22 @@
+import { getUrlParam } from '/frontend/src/pages/admin/edicao/functions/getUrlParam.js'
+import { makeRequest } from '/frontend/src/functions/makeRequest.js'
+import { navigateTo } from '/frontend/src/functions/navigateTo.js'
+import { ROUTES, API_ENDPOINTS } from '/frontend/src/utils/routes.js'
+
+export async function getAluno() {
+    try {
+        const accessToken = localStorage.getItem('accessToken')
+        const id = getUrlParam('id')
+        const response = await makeRequest({ 
+            url: API_ENDPOINTS.GET_USER(id), 
+            method:'GET', 
+            token: accessToken 
+        })
+        return response
+    } catch (error) {
+        if (error.status === 403) {
+            alert('Acesso Proibido')
+            navigateTo(ROUTES.ERROR404)
+        }
+    }
+}
