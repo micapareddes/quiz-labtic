07536bc7d6bbd79118c2df25543fec0bf12b1b7e
micapareddes <micapareddes@gmail.com>
2024-08-24 13:49:35 -0300

    Pagina de edicao de alunos completa

--- frontend/src/pages/admin/edicao/alunos/page.js
+++ frontend/src/pages/admin/edicao/alunos/page.js
@@ -1,17 +1,17 @@
-// Functions
+// Services and others
 import { ROUTES } from '/frontend/src/utils/routes.js'
 import { verifyUserAccess } from '/frontend/src/auth/verifyUserAccess.js'
+import { getDisciplinas } from '/frontend/src/pages/admin/service/getDisciplinas.js'
+import { getAlunoWithDisciplinas } from './service/getAlunoWithDisciplinas.js'
+import { editUser } from '../service/editUser.js'
+import { cadastroUserValidation } from '/frontend/src/validations/cadastroUserValidation.js'
+// Functions
 import { getUrlParam } from '/frontend/src/pages/admin/edicao/functions/getUrlParam.js'
 import { navigateTo } from '/frontend/src/functions/navigateTo.js'
-import { getDisciplinas } from '/frontend/src/pages/admin/service/getDisciplinas.js'
-import { getAluno } from './service/getAluno.js'
-import { removeOriginalValuesFromStorage } from '/frontend/src/pages/admin/edicao/functions/removeOriginalValuesFromStorage.js'
 import { saveOriginalValues } from '/frontend/src/pages/admin/edicao/functions/saveOriginalValues.js'
 import { obtainOriginalValuesFromStorage } from '/frontend/src/pages/admin/edicao/functions/obtainOriginalValuesFromStorage.js'
-import { alterarDisciplinaNoBanco } from '/frontend/src/pages/admin/edicao/disciplinas/service/alterarDisciplinaNoBanco.js'
-import { cadastroUserValidation } from '/frontend/src/validations/cadastroUserValidation.js'
+import { removeOriginalValuesFromStorage } from '/frontend/src/pages/admin/edicao/functions/removeOriginalValuesFromStorage.js'
 import { goBack } from '/frontend/src/functions/goBack.js'
-
 // Components
 import { Heading } from '/frontend/src/components/heading.js'
 import { SidebarAdmin } from '/frontend/src/pages/admin/components/sidebar-admin.js'
@@ -19,12 +19,17 @@ import { Button } from '/frontend/src/components/button.js'
 import { TextInput } from '/frontend/src/components/text-input.js'
 import { MultiSelect } from '/frontend/src/components/multi-select.js'
 import { InfoToaster, openToaster, closeToaster } from '/frontend/src/components/toaster.js'
+import { openDialog, AlertDialog } from '/frontend/src/components/dialog.js'
 import { ErrorMessage } from '/frontend/src/pages/login/components/error-message.js' //TODO: colocar em components de admin
+import { editedDisciplinasDoAluno } from './service/editDisciplinasDoAluno.js'
 
 async function handleSubmit(e) {
     e.preventDefault()
 
     const form = e.target
+    const multiselect = form.querySelector('#multiselect')
+    const selectedDisciplinas = multiselect.querySelectorAll('#option:checked')
+    const selectedDisciplinasArray = Array.from(selectedDisciplinas)
     const nameInput = form.querySelector('#name')
     const matriculaInput = form.querySelector('#matricula')
     const emailInput = form.querySelector('#email')
@@ -32,14 +37,19 @@ async function handleSubmit(e) {
     const matriculaInputContainer = form.querySelector('#matricula-container')
     const emailInputContainer = form.querySelector('#email-container')
     const submitButton = form.querySelector('#submit')
-    const { nome, email, matricula } = obtainOriginalValuesFromStorage()
-
-    let editedData = {
+    const { nome, email, matricula, disciplinas } = obtainOriginalValuesFromStorage()
+    const editedData = {
         nome: nameInput.value.trim(),
         matricula: matriculaInput.value.trim(),
         email: emailInput.value.trim(),
     }
-
+    const editedDisciplinas = selectedDisciplinasArray.map((disciplina) => {
+        return {
+            nome: disciplina.getAttribute('data-label'),
+            id: disciplina.value,
+        }
+    })
+    
     const { success, error } = cadastroUserValidation(editedData)
 
     if (!success) {
@@ -66,7 +76,13 @@ async function handleSubmit(e) {
         return
     }
 
-    const formMudou = nome !== editedData.nome  || email !== editedData.email  || matricula !== editedData.matricula
+    const arraysSaoIguais = (a1, a2) => {
+        if (a1.length !== a2.length) return false
+        return a1.every(obj1 => a2.some(obj2 => obj1.id === obj2.id))
+    }
+    const disciplinasMudaram = !arraysSaoIguais(editedDisciplinas, disciplinas)
+    
+    const formMudou = nome !== editedData.nome  || email !== editedData.email  || matricula !== editedData.matricula || disciplinasMudaram
 
     if (!formMudou) {
         openToaster(
@@ -78,22 +94,24 @@ async function handleSubmit(e) {
 
         return
     }
+console.log(editedDisciplinas);
 
-    // try {
-    //     await alterarDisciplinaNoBanco(editedData)
-    //     removeOriginalValuesFromStorage()
-    //     localStorage.setItem('disciplinaAlterada', editedData.nome)
-    //     navigateTo('disciplinas.html') 
-    // } catch (error) {
-    //     if (error.status === 403) {
-    //         alert('Acesso Proibido')
-    //         localStorage.removeItem('accessToken')
-    //         navigateTo('/frontend/src/pages/login.html')
-    //     } else {
-    //         console.log(error);
-    //         alert('Algo deu errado, tente novamente mais tarde...')
-    //     }
-    // }
+    try {
+        await editUser(editedData)
+        if (disciplinasMudaram) await editedDisciplinasDoAluno(editedDisciplinas)
+        removeOriginalValuesFromStorage()
+        localStorage.setItem('alunoAlterado', editedData.nome)
+        navigateTo(ROUTES.ADMIN.PAINEL.ALUNOS) 
+    } catch (error) {
+        if (error.status === 403) {
+            alert('Acesso Proibido')
+            localStorage.removeItem('accessToken')
+            navigateTo(ROUTES.LOGIN)
+        } else {
+            console.log(error);
+            alert('Algo deu errado, tente novamente mais tarde...')
+        }
+    }
 }
 
 function handleChange(event) {
@@ -120,8 +138,9 @@ async function EdicaoCadastroPage() {
     const form = document.createElement('form')
     const inputsContainer = document.createElement('div')
     const buttonContainer = document.createElement('div')
-    const disciplinas = await getDisciplinas()
-    const { email, matricula, nome } = await getAluno()
+    const disciplinasCadastradas = await getDisciplinas()
+    const { nome, matricula, email, disciplinas } = await getAlunoWithDisciplinas()
+    const disciplinasAlunoIds = new Set(disciplinas.map(disciplina => disciplina.id))
 
     inputsContainer.className = 'grid md:grid-cols-2 gap-8 items-start mt-10'
     buttonContainer.className = 'mt-auto text-center'
@@ -147,7 +166,7 @@ async function EdicaoCadastroPage() {
         MultiSelect({
             labelName: 'Disciplinas',
             placeholder: 'Disciplinas do aluno',
-            optionsArray: disciplinas,
+            optionsArray: disciplinasCadastradas,
             tooltip: 'Devem existir disciplinas cadastradas, logo o campo é opcional.'
         })
     )
@@ -165,19 +184,7 @@ async function EdicaoCadastroPage() {
         Heading({
             goBack: true, 
             title: 'Edição do Aluno', 
-            onGoBack: () => {
-                if (form.querySelector('input').value) {
-                    openDialog(
-                        AlertDialog({
-                            message: 'O cadastro não será salvo.',
-                            confirmarButtonName: 'Voltar',
-                            onConfirm: () => goBack()
-                        })
-                    )
-                    return
-                }
-                goBack()
-            }
+            onGoBack: () => goBack() //TODO: adicionar lógica de "alterações não serão salvas"
         }),
         form
     )
@@ -185,14 +192,24 @@ async function EdicaoCadastroPage() {
     const nameInput = form.querySelector('#name')
     const matriculaInput = form.querySelector('#matricula')
     const emailInput = form.querySelector('#email')
+    const multiselect = form.querySelector('#multiselect')
+    const options = multiselect.querySelectorAll('#option')
     nameInput.value = nome
     matriculaInput.value = matricula
     emailInput.value = email
+    options.forEach((option) => {
+        if (disciplinasAlunoIds.has(option.value)) {
+            option.checked = true
+            const event = new Event('change', { bubbles: true })
+            option.dispatchEvent(event)
+        }
+    })
 
     saveOriginalValues({
         nome,
         matricula,
         email,
+        disciplinas,
     })
 
     form.onsubmit = handleSubmit
