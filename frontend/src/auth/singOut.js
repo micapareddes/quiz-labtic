ddb28723d54b39a306122dce398388a51de1660e
micapareddes <micapareddes@gmail.com>
2024-08-26 09:36:31 -0300

    Eliminado comentarios de signOut.js

--- frontend/src/auth/singOut.js
+++ frontend/src/auth/singOut.js
@@ -1,21 +1,6 @@
 import { ROUTES } from '/frontend/src/utils/routes.js'
-const buttonEncerrarSessao = document.getElementById('encerrar-sessao-button')
-const buttonCancelarDialogEncerrarSessao = document.getElementById('cancelar-button-encerrar-sessao-dialog')
-const buttonConfirmarDialogEncerrarSessao = document.getElementById('confirmar-button-encerrar-sessao-dialog')
 
 export function signOut() {
     localStorage.removeItem('accessToken')
     window.location.href = ROUTES.LOGIN
 }
-
-// buttonEncerrarSessao.addEventListener('click', () => {
-//     openDialog('dialog-encerrar-sessao')
-// })
-
-// buttonCancelarDialogEncerrarSessao.addEventListener('click', () => {
-//     closeDialog('dialog-encerrar-sessao')
-// })
-
-// buttonConfirmarDialogEncerrarSessao.addEventListener('click', () => {
-//     signOut()
-// })
