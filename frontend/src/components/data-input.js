32d361a877af395d3b69bad0a42202ea831c127f
micapareddes <micapareddes@gmail.com>
2024-08-27 19:50:05 -0300

    Criado componente DataInput

---
+++ frontend/src/components/data-input.js
@@ -0,0 +1,15 @@
+export function DataInput({ labelName = 'Data', id }) {
+    const container = document.createElement('div')
+    const dataInput = document.createElement('input')
+    const label = document.createElement('label')
+
+    dataInput.type = 'date'
+    dataInput.className = 'bg-transparent pl-4 py-3 border-b border-stone-300 text-stone-500 hide-calendar-icon'
+    label.textContent = labelName
+    label.htmlFor = id
+    label.className = 'text-stone-500'
+    container.id = id
+    container.className = 'flex gap-2 items-center'
+    container.append(dataInput, label)
+    return container
+}
