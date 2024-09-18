e88fa10b5add64f3f6ad1280ec8a8a3b9bdbe8e9
micapareddes <micapareddes@gmail.com>
2024-09-18 13:39:28 -0300

    Adiciona ver gabarito em dialog ao finalizar quiz

--- frontend/src/components/dialog.js
+++ frontend/src/components/dialog.js
@@ -1,5 +1,5 @@
 import { Button } from './button.js'
-import { Title } from './fonts.js'
+import { Title, Text } from './fonts.js'
 
 function constructorDialog({ title, message, confirmarButtonName = 'Confirmar', onConfirm, color='neutral' }) {
     const container = document.createElement('dialog')
@@ -10,7 +10,7 @@ function constructorDialog({ title, message, confirmarButtonName = 'Confirmar', 
     const cancelarButton = document.createElement('button')
     const confirmarButton = document.createElement('button')
 
-    container.className = 'absolute left-0 top-0 bg-indigo-950 bg-opacity-50 backdrop-blur-sm h-screen w-screen flex items-center justify-center'
+    container.className = 'fixed left-0 top-0 bg-indigo-950 bg-opacity-50 backdrop-blur-sm h-screen w-screen flex items-center justify-center'
     container.id = 'dialog'
 
     div.className = 'bg-neutral-50 rounded-3xl px-6 py-8'
@@ -70,53 +70,42 @@ export function ActionDialog({ title, message, confirmarButtonName, onConfirm })
 }
 
 export function SuccessDialog({ 
-    title='Título', message='Mensagem do dialog', buttonName='Redirecionar', link,
+    title='Título', message='Mensagem do dialog', buttonName='Redirecionar', link, allowClose=true
 }) {
     const container = document.createElement('dialog')
     const div = document.createElement('div')
-    const dialog = document.createElement('dialog')
     const headingContainer = document.createElement('header')
     const icon = document.createElement('i')
     const dialogTitle = Title({
         as: 'h4',
         title,
     })
-    const dialogMessage = document.createElement('p')
+    const dialogMessage = Text({
+        text: message,
+        tone: 's-700',
+        size: 'md'
+    })
     const closeButton = document.createElement('button')
     const redirectButton = Button({
         variant: 'ghost',
         title: buttonName,
         link: link,
+        ariaLabel: `Botão de ${buttonName}`,
     })
 
-    container.className = 'absolute left-0 top-0 bg-indigo-950 bg-opacity-50 backdrop-blur-sm h-screen w-screen flex items-center justify-center'
+    container.className = 'fixed left-0 top-0 bg-indigo-950 bg-opacity-50 backdrop-blur-sm h-screen w-screen flex items-center justify-center'
     headingContainer.className = 'flex flex-row gap-2 items-center'
-    div.className = 'bg-neutral-50 rounded-3xl px-6 py-8'
+    div.className = 'bg-neutral-50 rounded-3xl px-6 pt-10 pb-10 flex flex-col items-center justify-center gap-4 min-w-96'
     icon.className = 'ph-fill ph-check-circle text-emerald-500 text-2xl'
+    closeButton.className = 'ph ph-x text-stone-900 opacity-20 w-full text-end'
 
+    if (allowClose) div.classList.add('pt-5')
+    closeButton.onclick = () => container.remove()
+
+    if (allowClose) div.appendChild(closeButton)
     headingContainer.append(icon, dialogTitle)
-    div.appendChild(headingContainer)
+    div.append(headingContainer, dialogMessage, redirectButton)
     container.appendChild(div)
-
-
-    // icon.src = '/frontend/src/img/icones/check-circle.svg'
-    // icon.height = 32
-    // icon.width = 32
-
-    // dialogTitle.textContent = title
-    // dialogTitle.className = 'text-xl lg:text-2xl font-semibold text-stone-900 mb-4'
-
-    // dialogMessage.textContent = message
-    // dialogMessage.className = 'mb-6 text-stone-700 text-base w-64 md:w-[350px] lg:w-[479px]'
-
-    // closeButton.src = '/frontend/src/img/icones/x.svg'
-    // closeButton.width = 21
-    // closeButton.height = 21
-    // closeButton.className = 'opacity-20'
-
-    // headingContainer.append(icon, dialogTitle)
-    // dialog.append(headingContainer, dialogMessage, redirectButton)
-    // container.appendChild(dialog)
 
     return container
 }
