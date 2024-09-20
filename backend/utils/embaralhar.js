730e096908f7bdb890d0c5847f632133fd7d2fce
micapareddes <micapareddes@gmail.com>
2024-09-20 13:54:31 -0300

    Resolve bug de embaralhar alternativas de quiz

--- backend/utils/embaralhar.js
+++ backend/utils/embaralhar.js
@@ -1,13 +1,14 @@
 export function embaralhar(array) {
     for (let indice = array.length; indice; indice--) {
+
         const indiceAleatorio = Math.floor(Math.random() * indice)
 
-        const elemento = array[indice - 1]
+        const elemento = array[indice - 1];
         
-        array[indice - 1] = array[indiceAleatorio]
+        array[indice - 1] = array[indiceAleatorio];
         
-        array[indiceAleatorio] = elemento
+        array[indiceAleatorio] = elemento;
 
-        return array
     }
+    return array
 }
