afff6a3d947e48e8c445278f56593af051dd1229
micapareddes <micapareddes@gmail.com>
2024-09-23 22:52:23 -0300

    Adiciona responsivo da sidebar

--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -10,24 +10,24 @@ function Item({
     const action = link ? document.createElement('a') : document.createElement('button')
     const icon = document.createElement('i')
     const text = document.createElement('p')
-
+    
     container.className = 'group'
 
     action.dataset.active = active
     action.classList = `
         group flex items-center gap-2 
-        py-1 md:py-0  md:px-8 
-        border-b-2 md:border-b-0 md:border-l-4 border-transparent hover:border-l-yellow-300 
-        data-[active=true]:border-l-yellow-200 data-[active=true]:border-b-yellow-200
+        py-1 px-2 md:py-0  md:px-8 
+        border-l-4 border-transparent enabled:hover:border-l-yellow-300 
+        md:data-[active=true]:border-l-yellow-200
     `
 
     if (link) action.href = link
     if (onClick) action.onclick = onClick
 
-    icon.classList = `ph ph-${phosphor} text-indigo-50 group-hover:text-yellow-200 text-xl`
+    icon.classList = `ph ph-${phosphor} text-indigo-50 group-data-[active=true]:text-yellow-200 md:group-data-[active=true]:text-indigo-50 text-sm md:text-xl`
 
     text.textContent = title
-    text.classList = 'hidden lg:block text-sm text-indigo-50 group-hover:text-yellow-200 group-data-[active=true]:font-semibold'
+    text.classList = 'block md:hidden lg:block text-xs lg:text-sm text-indigo-50 group-data-[active=true]:text-yellow-200 md:group-data-[active=true]:text-indigo-50 group-data-[active=true]:font-semibold'
 
     action.append(icon, text)
     container.appendChild(action)
@@ -36,11 +36,13 @@ function Item({
         const arrow = document.createElement('i')
         const options = document.createElement('ul')
 
-        arrow.className = 'ph ph-caret-right text-indigo-50 font-base group-hover:rotate-90 transition-all duration-200'
+        if (accordionOptions.length === 0) action.disabled = true
+
+        arrow.className = 'ph ph-caret-right text-indigo-50 font-base enabled:group-hover:rotate-90 transition-all duration-200'
 
         options.className = `hidden
             flex flex-col items-start justify-center pt-6 pl-8 space-y-4
-            [&_a]:block [&_a]:cursor-pointer [&_a]:text-indigo-50 [&_a]:text-sm
+            [&_a]:block [&_a]:cursor-pointer [&_a]:text-indigo-50 [&_a]:text-xs
         `
 
         accordionOptions.forEach((option) => {
@@ -59,8 +61,10 @@ function Item({
         action.addEventListener('click', (e) => {
             e.preventDefault()
 
-            options.classList.toggle('hidden')
-            arrow.classList.toggle('rotate-90')
+            if (accordionOptions.length > 0) {
+                options.classList.toggle('hidden')
+                arrow.classList.toggle('rotate-90')
+            }
         })
 
         action.appendChild(arrow)
@@ -78,9 +82,12 @@ export function Sidebar({ size='lg', items=[], changePassword=true }) {
     const sidebar = document.createElement('header')
     const smallLogo = document.createElement('img')
     const bigLogo = LogoLado()
+    const div = document.createElement('div')
     const nav = document.createElement('nav')
     const navList = document.createElement('ul')
     const footerItems = document.createElement('div')
+    const hamburguerMenu = document.createElement('button')
+    const hamburguerIcon = document.createElement('i')
     const logout = Item({
         phosphor: 'sign-out',
         title: size === 'lg' ? 'Encerrar sessão': '',
@@ -111,18 +118,27 @@ export function Sidebar({ size='lg', items=[], changePassword=true }) {
         footerItems.appendChild(trocarSenha)
     }
 
-    sidebar.className = `block md:h-screen w-full p-6 md:p-0 box-border flex flex-row md:flex-col ${containerStyle[size]} bg-indigo-950`
-    
+    sidebar.className = `block flex w-full flex-row justify-between md:flex-col p-6 md:h-screen md:p-0 box-border ${containerStyle[size]} bg-indigo-950`
+
+    div.className = 'flex flex-col w-full md:h-screen'
+
     smallLogo.src = '/frontend/src/img/logo-icon.svg'
     smallLogo.alt = 'Logo icone Polvo Branca'
     smallLogo.className = 'ml-9 md:pt-12'
     smallLogo.height = 21
     smallLogo.width = 21
 
-    nav.className = 'flex-1 w-full md:py-10'
-    navList.className = 'flex md:flex-col h-full gap-6'
+    nav.className = 'hidden mt-6 md:mt-0 md:block flex-1 w-full md:py-10'
+    navList.className = 'flex flex-col h-full gap-6'
 
-    footerItems.className = 'mt-auto flex items-center md:items-start md:flex-col gap-6'
+    hamburguerIcon.className = 'ph ph-list text-xl text-indigo-50'
+    hamburguerMenu.className = 'md:hidden text-end'
+
+    footerItems.className = 'mt-auto flex items-start flex-col gap-6'
+
+    hamburguerMenu.onclick = () => {
+        nav.classList.toggle('hidden')
+    }
 
     items.forEach((item) => {
         const sidebarItem = Item({
@@ -143,7 +159,9 @@ export function Sidebar({ size='lg', items=[], changePassword=true }) {
     footerItems.appendChild(logout)
     navList.appendChild(footerItems)
     nav.appendChild(navList)
-    sidebar.appendChild(nav)
+    hamburguerMenu.appendChild(hamburguerIcon)
+    div.append(hamburguerMenu, nav)
+    sidebar.appendChild(div)
 
     return sidebar
 }
